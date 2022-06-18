a0f7bda3d5a0f0a3ff1baa07e0d9de60cfe7387f
Akash Angadi <81625153+AKASHANGADII@users.noreply.github.com>
2022-06-18 16:50:15 +0530

    Add files via upload

---
+++ array.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Enter the array size>>"
+read n
+arr[$n]
+echo "Enter the array elements"
+for((i=0;i<n;i++))
+do 
+	read arr[i]
+done
+echo
+echo "The array elements are"
+for((i=0;i<n;i++))
+do 
+	read arr[i]
+done
